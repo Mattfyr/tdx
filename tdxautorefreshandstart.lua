331867185261e5b4a38cbde1619ce51b6dfb8cce
Mattfyr <mailyesyeslele@gmail.com>
2024-09-12 15:16:42 +0200

    hhh

--- tdxautorefreshandstart.lua
+++ tdxautorefreshandstart.lua
@@ -1,8 +1,8 @@
 if game.PlaceId == 9503261072 then
 while true do
     task.wait(1.5)
-    game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("/refresh")
-    task.wait(1.5)
-    game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("/start")
+    game:GetService("TextChatService").TextChannels:WaitForChild("RBXGeneral"):SendAsync("/refresh")
+    task.wait(2)
+    game:GetService("TextChatService").TextChannels:WaitForChild("RBXGeneral"):SendAsync("/start")
 end
 end
