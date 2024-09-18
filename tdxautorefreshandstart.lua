3c84fd29e0c8b6e74ef2665246705f6964445825
Mattfyr <mailyesyeslele@gmail.com>
2024-09-18 16:43:14 +0200

    fixed

--- tdxautorefreshandstart.lua
+++ tdxautorefreshandstart.lua
@@ -1,6 +1,6 @@
 if game.PlaceId == 9503261072 then
-    if game.PrivateServerId ~= "" or game.PrivateServerOwnerId ~= 0 then
-        while true do
+    while true do
+        if #game:GetService("Players"):GetPlayers() == 1 then
             if not game:GetService("Players").LocalPlayer.PlayerGui.GUI.Leave.Visible then
                 game:GetService("TextChatService").TextChannels:WaitForChild("RBXGeneral"):SendAsync("/refresh")
             else
@@ -8,7 +8,7 @@ if game.PlaceId == 9503261072 then
                     game:GetService("TextChatService").TextChannels:WaitForChild("RBXGeneral"):SendAsync("/start")
                 end
             end
-            task.wait(3)
         end
+        task.wait(3)
     end
 end
