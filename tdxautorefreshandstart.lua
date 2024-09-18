99f4cf4d69fafaad6a54d54fcf9c690f8d87a261
Mattfyr <mailyesyeslele@gmail.com>
2024-09-18 16:38:20 +0200

    priv server check

--- tdxautorefreshandstart.lua
+++ tdxautorefreshandstart.lua
@@ -1,12 +1,14 @@
 if game.PlaceId == 9503261072 then
-    while true do
-        if not game:GetService("Players").LocalPlayer.PlayerGui.GUI.Leave.Visible then
-            game:GetService("TextChatService").TextChannels:WaitForChild("RBXGeneral"):SendAsync("/refresh")
-        else
-            if game:GetService("Players").LocalPlayer.PlayerGui.GUI.Leave.Visible then
-                game:GetService("TextChatService").TextChannels:WaitForChild("RBXGeneral"):SendAsync("/start")
+    if game.PrivateServerId ~= "" or game.PrivateServerOwnerId ~= 0 then
+        while true do
+            if not game:GetService("Players").LocalPlayer.PlayerGui.GUI.Leave.Visible then
+                game:GetService("TextChatService").TextChannels:WaitForChild("RBXGeneral"):SendAsync("/refresh")
+            else
+                if game:GetService("Players").LocalPlayer.PlayerGui.GUI.Leave.Visible then
+                    game:GetService("TextChatService").TextChannels:WaitForChild("RBXGeneral"):SendAsync("/start")
+                end
             end
+            task.wait(3)
         end
-        task.wait(3)
     end
 end
